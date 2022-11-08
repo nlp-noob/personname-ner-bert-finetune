580510de19450ad6e48403d0c01de99d0d954e92
nlp-noob <jackyhu952@gmail.com>
2022-11-08 13:21:55 +0800

    second commit

--- eval_models.py
+++ eval_models.py
@@ -6,6 +6,9 @@ from transformers import AutoTokenizer, AutoModelForTokenClassification
 
 # 是否把sub-word全部标记的选项
 LABEL_ALL_TOKENS = True
+# 设置打标签的名字，分别对应前面的标签和后面的标签
+PREVIOUS_LABEL = "B-PER"
+CENTER_LABEL = "I-PER"
 
 def get_conversations_and_labels():
     file_list = os.listdir("conversation_data")
@@ -29,12 +32,12 @@ def get_conversations_and_labels():
                 index_list = line.split()
                 for i in range(len(index_list)):
                     index_list[i] = int(index_list[i])
-                label.appen(index_list)
+                label.append(index_list)
         label_list.append(label)
         conversation_list.append(conversation)
     return label_list, conversation_list
 
-def align_label(tokenized_input, labels, model):
+def align_label_b(tokenized_input, labels, model):
     word_ids = tokenized_input.word_ids()
     previous_word_idx = None
     label_ids = []
@@ -65,60 +68,95 @@ def get_predict_label(conversation, model, tokenizer):
         logits = model(**inputs).logits
     predicted_token_class_ids = logits.argmax(-1)
     predicted_tokens_classes = [model.config.id2label[t.item()] for t in predicted_token_class_ids[0]]
-    print(len(predicted_tokens_classes))
     return predicted_tokens_classes
 
+def align_label(sentence, label, tokenizer):
+    # 这里传入的label就是当前这句话中所有标记的坐标
+    # 如果label为空表示这里面没有标注
+    joined_sentence = " ".join(sentence)
+    inputs = tokenizer(joined_sentence, add_special_tokens=True, return_tensors="pt")
+    word_ids = inputs.word_ids()
+    tokenized_sentence = tokenizer.convert_ids_to_tokens(inputs["input_ids"][0])
+    # 计算坐标的偏移
+    label_bias = 0
+    for token_id in range(len(tokenized_sentence)):
+        if tokenized_sentence[token_id]==None:
+            continue
+        if tokenized_sentence[token_id]==":":
+            label_bias = word_ids[token_id]
+            break
+    aligned_label = []
+    for token_ids in range(len(tokenized_sentence)):
+        aligned_label.append("O")
+    for a_label in label:
+        for label_index in range(len(a_label)):
+            if(label_index==0):
+                label_name = PREVIOUS_LABEL
+            else:
+                label_name = CENTER_LABEL
+            for label_to_modify_index in range(len(aligned_label)):
+                if word_ids[label_to_modify_index]==a_label[label_index]+label_bias:
+                   aligned_label[label_to_modify_index]=label_name
+    return aligned_label
+
 # 用来产生文本形式的输入输出的函数
-def modify_inputs_and_labels_text1(conversation_list, label_list):
+def modify_inputs_and_labels_text1(conversation_list, label_list, tokenizer):
     inputs = []
     labels = []
     for i in range(len(conversation_list)):
         conversation = conversation_list[i]
         label = label_list[i]
         user_indexs = []
+        aligned_labels = []
+        # 将label转换成tokenized的形式
+        for index in range(len(conversation)):
+            label_to_convert = []
+            for one_label in label:
+                if(len(one_label)>0 and one_label[0]==(index+1)):
+                    a_label_in_sentence = []
+                    for label_index in range(len(one_label)):
+                        if label_index==0:
+                            continue
+                        else:
+                            a_label_in_sentence.append(one_label[label_index])
+                    label_to_convert.append(a_label_in_sentence)
+            aligned_labels.append(align_label(conversation[index],label_to_convert,tokenizer))
+        ##############################
         for index in range(len(conversation)):
             if(conversation[index][0]=="[USER]"):
                 user_indexs.append(index)
         for index in user_indexs:
             if(index==0 and len(conversation)>1):
                 a_sen = conversation[index]
-                a_sen.append("\n")
                 b_sen = conversation[index+1]
                 a_sen.extend(b_sen)
                 sen = a_sen
-                a_label = label[index]
-                a_label.append("O")
-                b_label = label[index+1]
+                a_label = aligned_labels[index]
+                b_label = aligned_labels[index+1]
                 a_label.extend(b_label)
                 sen_label = a_label
             elif(index==0 and len(conversation)==1):
                 sen = conversation[index]
-                sen_label = label[index]
+                sen_label = aligned_labels[index]
             elif(index==len(conversation)-1):
                 a_sen = conversation[index-1]
-                a_sen.append("\n")
                 b_sen = conversation[index]
                 a_sen.extend(b_sen)
                 sen = a_sen
-                a_label = label[index-1]
-                a_label.append("O")
-                b_label = label[index]
+                a_label = aligned_labels[index-1]
+                b_label = aligned_labels[index]
                 a_label.extend(b_label)
                 sen_label = a_label
             else:
                 a_sen = conversation[index-1]
-                a_sen.append("\n")
                 b_sen = conversation[index]
-                b_sen.append('\n')
                 c_sen = conversation[index+1] 
                 a_sen.extend(b_sen)
                 a_sen.extend(c_sen)
                 sen = a_sen 
-                a_label = label[index-1]
-                a_label.append("O")
-                b_label = label[index]
-                b_label.append('O')
-                c_label = label[index+1] 
+                a_label = aligned_labels[index-1]
+                b_label = aligned_labels[index]
+                c_label = aligned_labels[index+1] 
                 a_label.extend(b_label)
                 a_label.extend(c_label)
                 sen_label = a_label
@@ -133,11 +171,15 @@ def main():
     tokenizer = AutoTokenizer.from_pretrained(model_path)
     model = AutoModelForTokenClassification.from_pretrained(model_path)
     labels, conversations = get_conversations_and_labels()
-    inputs, true_labels = modify_inputs_and_labels_text1(conversations,labels)
+    print(labels)
     print("There are {} conversations".format(len(conversations)))
     print("There are {} labels".format(len(labels)))
+    inputs, true_labels = modify_inputs_and_labels_text1(conversations,labels,tokenizer)
     for i in range(len(inputs)): 
-        predict_label = get_predict_label(inputs[i], model, tokenizer)
-
+        predict_labels = get_predict_label(inputs[i], model, tokenizer)
+        if len(true_labels[i])!=len(predict_labels):
+            print("true label len:{}, predict label len:{}".format(len(true_labels[i]),len(predict_labels)))
+            print(i)
+    import pdb;pdb.set_trace()
 if __name__=="__main__":
     main()
